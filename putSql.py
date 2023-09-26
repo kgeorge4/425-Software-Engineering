088f186d0a63791a1db5f66cc60580db009a2417
jeffrey <jeff.kretschmer@gmail.com>
2023-09-26 10:23:41 -0400

    commit

---
+++ putSql.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+
+# read from JSON to get the 
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
